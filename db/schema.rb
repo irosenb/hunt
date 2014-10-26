4855fe7a7c017e21f36c64d7349f33f5137a6d4a
Coulton Vento <coultonvento@gmail.com>
2014-10-25 19:40:42 -0700

    Db migration 👌

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026001252) do
+ActiveRecord::Schema.define(version: 20141026023146) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141026001252) do
     t.string   "integer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location"
   end
 
 end
