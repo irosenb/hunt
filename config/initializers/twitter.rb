512e6f5ba18a6a43e1440f9ea84452531b15e762
Coulton Vento <coultonvento@gmail.com>
2014-10-25 18:00:06 -0700

    Updated user.rake

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 tw_client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "#{tw_api_key}"
-  config.consumer_secret     = "#{tw_api_secret}"
-  config.access_token        = "#{tw_dev_token}"
-  config.access_token_secret = "#{tw_dev_secret} "
+  config.consumer_key        = "#{ENV['tw_api_key']}"
+  config.consumer_secret     = "#{ENV['tw_api_secret']}"
+  config.access_token        = "#{ENV['tw_dev_token']}"
+  config.access_token_secret = "#{ENV['tw_dev_secret']} "
 end
--- lib/tasks/users.rake
+++ lib/tasks/users.rake
@@ -1,16 +1,14 @@
 namespace :users do
 
   task get: :environment do
-  	
   	response = Unirest.get "https://api.producthunt.com/v1/posts",
   		headers: {
   			"Authorization" => "Bearer #{ENV["ph_dev_token"]}",
   		}
+  end
+
+  task locations: :environment do
+  	puts User.all
+  end
   
-  end
-
-  task :locations do
-
-  end
-
 end
