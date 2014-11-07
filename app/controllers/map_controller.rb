56c57c677a0bec704fee936c8581e3c65707193e
Isaac Rosenberg <irosenb7@gmail.com>
2014-11-07 00:12:18 -0800

    popup

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,30 +1,37 @@
 class MapController < ApplicationController
-  def show
-    @users = User.all
+  def index
+    @users = User.includes(:products).where.not(latitude: nil)
+    # @products = Product.all
     @json = []
 
     @users.each do |user|
-      if !user.latitude.nil?
-        @json << {
-        type: "Feature",
-           geometry: {
-             type: "Point",
-             coordinates: [user.longitude, user.latitude]
-           },
-           properties: {
-            name: user.name,
-            username: user.username,
-            product: user.products.first.to_json,
-            title: "<a target='_blank' href='http://twitter.com/#{user.username}'>#{user.name}</a>"
-          }
+      html = "<a href='#{user.products.first.link}'>#{user.products.first.name}</a> by <a href='http://twitter.com/#{user.username}'>#{user.name}</a>"
+      @json << {
+      type: "Feature",
+         geometry: {
+           type: "Point",
+           coordinates: [user.longitude, user.latitude]
+         },
+         properties: {
+          name: user.name,
+          username: user.username,
+          product: user.products.first.to_json,
+          title: html
         }
-      end
+      }
     end
 
     respond_to do |format|
       format.html 
       format.js
       format.json { render json: @json } 
+    end
+  end
+
+  def popup
+    @users = User.includes(:products).where(username: params[:usernames])
+    respond_to do |format|
+      format.html { render partial: "popup" }
     end
   end
 end
