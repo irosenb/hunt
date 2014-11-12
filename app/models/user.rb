f75c5d62f916567b0c2461759d75048da1a0e7ef
Isaac Rosenberg <irosenb7@gmail.com>
2014-11-11 21:44:26 -0800

    huge refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,62 @@
 class User < ActiveRecord::Base
   has_many :products
+  scope :no_location, -> { where(latitude: nil) }
+  scope :lowercased, ->(screen_name) { where("lower(username) = ?", screen_name.downcase).first }
+  # scope :
+
+  def locations
+    users = User.no_location
+    users.map(&:username).each_slice(100).to_a.each do |group|
+      puts user_query = group.join(",")
+      twitter_users = TW_CLIENT.users(user_query)
+      twitter_users.each do |twitter_user|
+        puts twitter_user.screen_name
+        user = User.lowercased
+        location = Geokit::Geocoders::MultiGeocoder.geocode(twitter_user.location)
+        user.location = twitter_user.location
+        user.latitude = location.latitude
+        user.longitude = location.longitude
+        puts user.save!
+      end
+    end
+  end
+
+  def update
+    ActiveRecord::Base.transaction do
+      
+    end
+  end
+
+  def get
+    ActiveRecord::Base.transaction do
+      get_users { |chunk, i| puts i[:name] }
+      # User.create(chunk)
+    end
+  end
+
+  def get_products
+    users = User.includes(:products).where.not(latitude: nil).where(products: {id: nil})
+    users.each do |user|
+      response = Unirest.get "https://api.producthunt.com/v1/users/#{user.username}", headers: { "Authorization" => "Bearer #{ENV["ph_dev_token"]}" }
+      response.body["user"]["maker_of"].each do |p| 
+        puts p["redirect_url"]
+        puts user.products.build(link: p["redirect_url"], name: p["name"])
+      end
+      puts user.save  
+    end
+  end
+
+
+  def get_users
+    SmarterCSV.process("./lib/users.csv", {:col_sep => ";", :quote_char => ";", :key_mapping => {:name => :name, :username => :username, :id => :hunt_id, :created_at => :hunted_at, :maker_of_count => :maker_count, :image => :image}, :remove_unmapped_keys => true}) do |chunk| 
+      chunk.each do |i|
+        maker = i[:maker_count] || 0
+        if maker.to_i > 0
+          i.delete(:maker_count)
+          yield(chunk, i) if block_given? 
+        end
+      end    
+    end
+  end
+
 end
--- lib/tasks/users.rake
+++ lib/tasks/users.rake
@@ -1,50 +1,19 @@
 namespace :users do
-  desc "Getting users locations from Twitter"
+  # desc "Getting users locations from Twitter"
+  
   task locations: :environment do
-  	users = User.all
-    users.map(&:username).each_slice(100).to_a.each do |group|
-      puts twis = group.join(",")
-      tw_users = TW_CLIENT.users(twis)
-      tw_users.each do |tw_user|
-        puts tw_user.screen_name
-        user = User.where("lower(username) = ?", tw_user.screen_name.downcase).first
-        location = Geokit::Geocoders::MultiGeocoder.geocode(tw_user.location)
-        user.latitude = location.latitude
-        user.longitude = location.longitude
-        puts user.save!
-      end
-    end
+  	User.locations
 	end
 
+  task update: :environment do
+  end
+
   task get: :environment do
-    ActiveRecord::Base.transaction do
-      get_users {|chunk, i| puts chunk.first["name"]}
-      # User.create(chunk)
-    end
+    User.get
   end
 
-  task retrieve: :environment do
-    users = User.where.not(latitude: nil)
-    users.each do |user|
-      response = Unirest.get "https://api.producthunt.com/v1/users/#{user.username}", headers: { "Authorization" => "Bearer #{ENV["ph_dev_token"]}" }
-      response.body["user"]["maker_of"].each do |p| 
-        puts p["redirect_url"]
-        puts user.products.build(link: p["redirect_url"], name: p["name"])
-      end
-      puts user.save  
-    end
+  task get_products: :environment do
+    User.connection
+    User.get_products
   end
 end
-
-def get_users
-  SmarterCSV.process("./lib/users.csv", {:col_sep => ";", :quote_char => ";", :key_mapping => {:name => :name, :username => :username, :id => :hunt_id, :created_at => :hunted_at, :maker_of_count => :maker_count, :image => :image}, :remove_unmapped_keys => true}) do |chunk| 
-    chunk.each do |i|
-      maker = i[:maker_count] || 0
-      if maker.to_i > 0
-        i.delete(:maker_count)
-        yield(chunk, i) if block_given? 
-      end
-    end    
-  end
-end
-
