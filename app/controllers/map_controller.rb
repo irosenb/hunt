3283a85784fc9bf84258fd25561c2c478d6e7d40
Isaac Rosenberg <irosenb7@gmail.com>
2014-11-11 17:43:38 -0800

    cleanup

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,25 +1,7 @@
 class MapController < ApplicationController
   def index
     @users = User.includes(:products).where.not(latitude: nil)
-    @json = []
-
-    @users.each do |user|
-      html = "<a href='#{user.products.first.link}'>#{user.products.first.name}</a> by <a href='http://twitter.com/#{user.username}'>#{user.name}</a>"
-      @json << {
-      type: "Feature",
-         geometry: {
-           type: "Point",
-           coordinates: [user.longitude, user.latitude]
-         },
-         properties: {
-          name: user.name,
-          username: user.username,
-          product: user.products.first.to_json,
-          title: html
-        }
-      }
-    end
-
+    
     respond_to do |format|
       format.html 
       format.js
